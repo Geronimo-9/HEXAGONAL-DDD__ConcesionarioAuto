5e4577547ad8daa0486bd5162457db4c90e97802
geronimo-9 <gmijaresa@unicartagena.edu.co>
2026-05-30 09:01:02 -0500

    Feat: Vendedor VO domain implement

--- src/co/edu/udec/ConcesionarioDeAuto/domain/valueobject/Vendedor/VendedorSegundoNombre.java
+++ src/co/edu/udec/ConcesionarioDeAuto/domain/valueobject/Vendedor/VendedorSegundoNombre.java
@@ -1,4 +1,21 @@
 package edu.udec.ConcesionarioDeAuto.domain.valueobject.Vendedor;
 
-public class VendedorSegundoNombre {
+
+import edu.udec.ConcesionarioDeAuto.domain.exceptions.CamposInvalidosExcepcion;
+
+import java.util.Objects;
+
+public record VendedorSegundoNombre(String value) {
+
+    public VendedorSegundoNombre {
+
+        final String valorNormalizado =
+                Objects.requireNonNull(value).trim();
+
+        if (valorNormalizado.isEmpty()) {
+            throw CamposInvalidosExcepcion.camposObligatoriosVacios();
+        }
+
+        value = valorNormalizado;
+    }
 }
