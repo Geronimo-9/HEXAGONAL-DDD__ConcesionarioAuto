b5fa3b37ad16c133e331cfc15e63dfe8fb9f70b9
geronimo-9 <gmijaresa@unicartagena.edu.co>
2026-05-30 04:49:45 -0500

    Refactor: Enums Status Mecanico domain

--- src/co/edu/udec/ConcesionarioDeAuto/domain/enums/MecanicoEspecialidades.java
+++ src/co/edu/udec/ConcesionarioDeAuto/domain/enums/MecanicoEspecialidades.java
@@ -1,9 +1,9 @@
 package edu.udec.ConcesionarioDeAuto.domain.enums;
 
 public enum MecanicoEspecialidades {
-    AUTOMOTRIZ,
-    TECNICO,
-    INGENIERO,
-    CORRESIVO,
-
+    MECANICA_GENERAL,
+    ELECTRICIDAD_AUTOMOTRIZ,
+    LATONERIA_Y_PINTURA,
+    SUSPENSION_Y_FRENOS,
+    DIAGNOSTICO_ELECTRONICO
 }
